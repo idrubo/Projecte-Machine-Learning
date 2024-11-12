e341504f8e192e981945efbfceac389fa5c7ab0f
LLuís <ifikeen@gmail.com>
2024-11-12 16:24:34 +0100

    App deployment testing.

--- S9/bank_app.py
+++ S9/bank_app.py
@@ -61,32 +61,50 @@ st.write (f'poutcome is ', poutcome)
 #  12  job_freelance
 #  13  job_other
 job = st.selectbox ("job: ", ['non-qualified', 'qualified', 'semi-qualified', 'freelance', 'other'])
-
-match job:
-    case 'non-qualified':
-        st.write ("Job is non-qualified.")
-    case 'qualified':
-        st.write ("Job is qualified.")
-    case 'semi-qualified':
-        st.write ("Job is semi-qualified.")
-    case 'freelance':
-        st.write ("Job is freelance.")
-    case 'other':
-        st.write ("Job is other.")
+st.write (f'job is ', job)
 
 #  14  education_primary
 #  15  education_secondary
 #  16  education_tertiary
 #  17  education_unknown
 education = st.selectbox ("ecucation: ", ['primary', 'secondary', 'tertiary', 'unknown'])
+st.write (f'education is ', education)
 
-match education:
-    case 'primary':
-        st.write ("Education is primary.")
-    case 'secondary':
-        st.write ("Education is secondary.")
-    case 'tertiary':
-        st.write ("Education is tertiary.")
-    case 'unknown':
-        st.write ("Education is unknown.")
+#  #   Column               Non-Null Count  Dtype
+# ---  ------               --------------  -----
+#  0   age                  7813 non-null   float64
+#  1   balance              7813 non-null   float64
+#  2   campaign             7813 non-null   float64
+#  3   pdays                7813 non-null   float64
+#  4   contact              7813 non-null   int64
+#  5   default              7813 non-null   int64
+#  6   housing              7813 non-null   int64
+#  7   loan                 7813 non-null   int64
+#  8   poutcome             7813 non-null   int64
+#  9   job_non-qualified    7813 non-null   bool
+#  10  job_qualified        7813 non-null   bool
+#  11  job_semi-qualified   7813 non-null   bool
+#  12  job_freelance        7813 non-null   bool
+#  13  job_other            7813 non-null   bool
+#  14  education_primary    7813 non-null   bool
+#  15  education_secondary  7813 non-null   bool
+#  16  education_tertiary   7813 non-null   bool
+#  17  education_unknown    7813 non-null   bool
+
+# Crear un DataFrame con las entradas
+user_data = pd.DataFrame({
+    'age'      : [age],
+    'balance'  : [balance],
+    'campaign' : [campaign],
+    'pdays'    : [pdays],
+    'contact'  : [contact],
+    'default'  : [default],
+    'housing'  : [housing],
+    'loan'     : [loan],
+    'poutcome' : [poutcome],
+    'job'      : [job],
+    'education': [education]
+})
+
+st.write (user_data)
 
