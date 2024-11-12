0257d27c48fe8303c68fbb616cce6ee964e1c829
LLuís <ifikeen@gmail.com>
2024-11-12 11:47:06 +0100

    App deployment testing.

--- S9/bank_app.py
+++ S9/bank_app.py
@@ -14,3 +14,11 @@ with open('S9/bank_scaler.pkl', 'rb') as scaler_file:
 st.title ('Predicción Sobre la Contratación de un Depósito a Plazo Fijo.')
 st.header ('(Regresión Logística)')
 
+# Selection box
+
+# first argument takes the title of the selectionbox, second argument takes options
+job = st.selectbox("Job: ", ['admin.', 'engineer', 'manager'])
+
+# print the selected hobby
+st.write("Your job is: ", job)
+
