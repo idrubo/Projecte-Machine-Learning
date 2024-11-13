133ca531afb9aba986970bdcd8faf0bfef8a0a5a
LLuís <ifikeen@gmail.com>
2024-11-13 15:41:03 +0100

    App deployment testing.

--- S9/bank_app.py
+++ S9/bank_app.py
@@ -70,27 +70,6 @@ st.write (f'job is ', job)
 education = st.selectbox ("ecucation: ", ['primary', 'secondary', 'tertiary', 'unknown'])
 st.write (f'education is ', education)
 
-#  #   Column               Non-Null Count  Dtype
-# ---  ------               --------------  -----
-#  0   age                  7813 non-null   float64
-#  1   balance              7813 non-null   float64
-#  2   campaign             7813 non-null   float64
-#  3   pdays                7813 non-null   float64
-#  4   contact              7813 non-null   int64
-#  5   default              7813 non-null   int64
-#  6   housing              7813 non-null   int64
-#  7   loan                 7813 non-null   int64
-#  8   poutcome             7813 non-null   int64
-#  9   job_non-qualified    7813 non-null   bool
-#  10  job_qualified        7813 non-null   bool
-#  11  job_semi-qualified   7813 non-null   bool
-#  12  job_freelance        7813 non-null   bool
-#  13  job_other            7813 non-null   bool
-#  14  education_primary    7813 non-null   bool
-#  15  education_secondary  7813 non-null   bool
-#  16  education_tertiary   7813 non-null   bool
-#  17  education_unknown    7813 non-null   bool
-
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
     'age'                : [age],
