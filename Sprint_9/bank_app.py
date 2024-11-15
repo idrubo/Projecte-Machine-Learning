b35f5e752c3be842a0570ce77238c19725934e81
LLuís <ifikeen@gmail.com>
2024-11-15 16:11:08 +0100

    Actualización del directorio raiz en la app del Sprint 9.

--- Sprint_9/bank_app.py
+++ Sprint_9/bank_app.py
@@ -4,10 +4,10 @@ import pickle
 import pandas as pd
 
 # Cargar el modelo y el escalador desde archivos
-with open('S9/bank_model.pkl', 'rb') as model_file:
+with open('Sprint_9/bank_model.pkl', 'rb') as model_file:
     model = pickle.load (model_file)
 
-with open('S9/bank_scaler.pkl', 'rb') as scaler_file:
+with open('Sprint_9/bank_scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load (scaler_file)
 
 # Título de la aplicación
